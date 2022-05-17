dfa03578989c7faebe71cb4ee795167b9ace30f4
Stephan Schöpe <socialcrap+github@posteo.de>
2022-05-17 08:54:46 +0200

    docker

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,8 +3,7 @@
 if [ $NODE_ENV == "production" ]
 then
     echo "Running in production mode"
-    exec yarn install
-    exec yarn start 
+    exec yarn install && yarn start
 else
     echo "Running in development mode"
     exec yarn dev
