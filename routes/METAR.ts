f0986efb7fee1eda0a8796c791e1cd931ab21d70
Stephan Schöpe <socialcrap+github@posteo.de>
2022-05-31 17:55:17 +0200

    new metar route

--- routes/METAR.ts
+++ routes/METAR.ts
@@ -8,7 +8,61 @@ import metarParser from "metar-parser";
 
 const router = express.Router();
 
+// Find all from date
+// router.get("/", async (req: Request, res: Response) => {
+//   if (!req.query.lat || !req.query.long)
+//     return res.status(400).send("Wrong coordinate format (?lat=50&long=4)");
+
+//   // Find station by coordinates
+//   try {
+//     const response = await IcaoDataModel.findOne({
+//       location: {
+//         $near: {
+//           $geometry: {
+//             type: "Point",
+//             coordinates: [req.query.long, req.query.lat],
+//           },
+//         },
+//       },
+//     });
+//     if (!response)
+//       return res.status(400).send("No station found for this location");
+
+//     if (typeof req.query.date != "string" || !req.query.date)
+//       return res.status(400).send("Date is not a string (?date=2022-12-12)");
+
+//     // Find station by date
+//     const date = new Date(req.query.date);
+
+//     const metarData = await MetarDataModel.find({
+//       ICAO: response.ICAO,
+//       createdAt: {
+//         $gte: startOfDay(date),
+//         $lte: endOfDay(date),
+//       },
+//     });
+
+//     if (!metarData) return res.status(400).send("Such emtpy");
+
+//     const decoded = metarData.map((v) => {
+//       return metarParser(v.rawMetar);
+//     });
+
+//     res.status(201).send(decoded);
+//   } catch (error) {
+//     console.error(error);
+//     res.status(400).json("Error: " + error);
+//   }
+// });
+
+router.get("/decoded", async (req: Request, res: Response) => {
+  getMetarData(req, res, true);
+});
 router.get("/", async (req: Request, res: Response) => {
+  getMetarData(req, res);
+});
+
+async function getMetarData(req: Request, res: Response, decoded?: boolean) {
   if (!req.query.lat || !req.query.long)
     return res.status(400).send("Wrong coordinate format (?lat=50&long=4)");
 
@@ -33,25 +87,26 @@ router.get("/", async (req: Request, res: Response) => {
     // Find station by date
     const date = new Date(req.query.date);
 
-    const metarData = await MetarDataModel.find({
+    const metarData = await MetarDataModel.findOne({
       ICAO: response.ICAO,
       createdAt: {
-        $gte: startOfDay(date),
         $lte: endOfDay(date),
+        $gte: date,
       },
     });
 
     if (!metarData) return res.status(400).send("Such emtpy");
 
-    const decoded = metarData.map((v) => {
-      return metarParser(v.rawMetar);
-    });
+    if (decoded) {
+      const decoded = metarParser(metarData.rawMetar);
 
-    res.status(201).send(decoded);
+      res.status(201).send(decoded);
+    } else {
+      res.status(201).send(metarData.rawMetar);
+    }
   } catch (error) {
     console.error(error);
     res.status(400).json("Error: " + error);
   }
-});
-
+}
 export default router;
