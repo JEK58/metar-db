e8e5c6085d923c23ca77d5332a3a74484c6afb70
Stephan Schöpe <socialcrap+github@posteo.de>
2022-11-24 13:44:24 +0100

    add station service added

--- service/IcaoService.ts
+++ service/IcaoService.ts
@@ -1,4 +1,5 @@
 import IcaoDataModel from "../models/IcaoDataModel";
+import { IcaoDataCreate } from "../types/IcaoData";
 
 export async function getIcaoStationsFromDb(): Promise<string[]> {
   return (await IcaoDataModel.find().select("ICAO")).map((x) => x.ICAO);
@@ -14,4 +15,13 @@ export async function getInactiveIcaoStationsFromDb(): Promise<string[]> {
   return (await IcaoDataModel.find({ active: false }).select("ICAO")).map(
     (x) => x.ICAO
   );
+}
+
+export async function getIcaoDetails(ICAO: string) {
+  return await IcaoDataModel.findOne({ ICAO }).exec();
+}
+
+export async function addStation(station: IcaoDataCreate) {
+  const icaoData = new IcaoDataModel(station);
+  return await icaoData.save();
 }
