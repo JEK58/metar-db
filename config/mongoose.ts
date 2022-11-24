a4c9328f0e64c5e98029ba95569db84408ebc3fa
Stephan Schöpe <socialcrap+github@posteo.de>
2022-11-24 13:43:58 +0100

    monggose put in a function

--- config/mongoose.ts
+++ config/mongoose.ts
@@ -3,22 +3,26 @@ import mongoose from "mongoose";
 const uri = process.env.DB_CONNECTION;
 const dbName = process.env.MONGO_DB_NAME;
 
-// Initial connection
-try {
-  if (!uri) throw Error("No DB_CONNECTION env variable found");
+export async function initializeDB() {
+  // Initial connection
+  try {
+    if (!uri) throw Error("No DB_CONNECTION env variable found");
 
-  // Allow undefined dbName in development to use test DB
-  if (!dbName && process.env.NODE_ENV === "production")
-    throw Error("No DB_CONNECTION env variable found");
+    // Allow undefined dbName in development to use test DB
+    if (!dbName && process.env.NODE_ENV === "production")
+      throw Error("No DB_CONNECTION env variable found");
 
-  mongoose.connect(uri, { dbName });
-} catch (error) {
-  console.error(error);
+    return await mongoose.connect(uri, { dbName });
+  } catch (error) {
+    console.error(error);
+  }
+  // Logs connection errors after initial connection
+  mongoose.connection.on("error", (err: Error) => {
+    console.error(err);
+  });
 }
-// Logs connection errors after initial connection
-mongoose.connection.on("error", (err: Error) => {
-  console.error(err);
-});
+
+initializeDB();
 
 const mongodb = mongoose.connection;
 mongodb.on("error", () => console.error("connection error:"));
